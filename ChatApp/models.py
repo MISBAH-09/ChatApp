791c4e143ec7d16b5e8738311d904948f15d4c67
Misbah Sehar <misbahsahar09@gmail.com>
2026-01-12 12:32:23 +0500

    add password field and add validations to rest of the fields

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class User(models.Model):  # Existing, adjust if named differently
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=20 , unique=True)
+    email = models.EmailField(max_length=100 , unique=True) 
+    password = models.CharField(max_length=100)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     token = models.CharField(max_length=200, null=True, blank=True)
--- ChatApp/views/users.py
+++ ChatApp/views/users.py
@@ -2,12 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.contrib.auth.hashers import check_password , make_password
 
 from ChatApp import settings
 from ..models import User
 import hashlib
 import base64
 import os
+import re
 from django.core.files import File
 
 def save_base64_image(base64_data, filename):
@@ -41,27 +46,51 @@ class signupAPI(APIView):
       email = request.data.get('email')
       first_name = request.data.get('first_name', '')
       last_name = request.data.get('last_name', '')
+      password = request.data.get('password')
       profile = request.data.get('profile')
       
       # Validate required fields
-      if not username or not email:
-        response_data['message'] = 'username and email are required'
+      if not username or not password or not email:
+        response_data['message'] = 'username, password, and email are required'
         return Response(response_data, status=http_status)
         
+      #username validation
       if User.objects.filter(username=username).exists():
         response_data['message'] = 'Username already exists'
         return Response(response_data, status=http_status)
-        
-      if User.objects.filter(email=email).exists():
+      
+      valid_username, message = Validations().isvalidusername(username)
+      if not valid_username:
+        response_data['message'] = message
+        return Response(response_data, status=http_status)
+
+      #email validation
+      result = validate_email(email)
+      if result is not None:
+        response_data['message'] = 'Invalid email format'
+        return Response(response_data, status=http_status)
+      elif User.objects.filter(email=email).exists():
         response_data['message'] = 'Email already exists'
         return Response(response_data, status=http_status)
       
-      # Create user with current timestamp
+      #first name and last name validation
+      if first_name:
+        valid_firstname, message = Validations().isvalidName(first_name)
+        if not valid_firstname:
+          response_data['message'] = 'Invalid first name: ' + message
+          return Response(response_data, status=http_status)
+      if last_name:
+        valid_lastname, message = Validations().isvalidName(last_name)
+        if not valid_lastname:
+          response_data['message'] = 'Invalid last name: ' + message
+          return Response(response_data, status=http_status)
+      
       user = User.objects.create(
         username=username,
         email=email,
         first_name=first_name,
         last_name=last_name,
+        password=make_password(password),
         profile=profile
       )
       
@@ -93,33 +122,35 @@ class loginAPI(APIView):
       'data': None
     }
     http_status = status.HTTP_400_BAD_REQUEST
-    
+
     try:
       username = request.data.get('username')
       email = request.data.get('email')
+      password = request.data.get('password')
 
-      if not username or not email:
-        response_data['message'] = 'username and email are required'
+      if not password or (not username and not email):
+        response_data['message'] = 'Provide username or email and password'
         return Response(response_data, status=http_status)
-        
-      try:
-        user = User.objects.get(username=username)  
-      except User.DoesNotExist:
+
+      user = None
+      if username:
+        user = User.objects.filter(username=username).first()
+      elif email:
+        user = User.objects.filter(email=email).first()
+
+      if not user:
         response_data['message'] = 'Invalid username or email'
         return Response(response_data, status=http_status)
 
-      if user.email != email:
-        response_data['message'] = 'Invalid username or email'
+      if not check_password(password, user.password):
+        response_data['message'] = 'Invalid password'
         return Response(response_data, status=http_status)
-      
-      # Generate token
+
       raw_string = f"{user.id}{user.username}"
       token = hashlib.sha256(raw_string.encode()).hexdigest()
-      
-      if(token):
-         # Add token to database
-        result= self.add_token_intodb(user.id, token)
 
+      if token:
+        result = self.add_token_intodb(user.id, token)
         if result:
           response_data['success'] = True
           response_data['message'] = 'Login successful'
@@ -141,23 +172,21 @@ class loginAPI(APIView):
       else:
         response_data['message'] = 'Token generation failed'
         http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
-    
-      
+
     except Exception as e:
       response_data['message'] = str(e)
       http_status = status.HTTP_400_BAD_REQUEST
-    
+
     return Response(response_data, status=http_status)
 
   def add_token_intodb(self, id, token):
-    if User.objects.filter(id=id).exists():
-        user = User.objects.get(id=id)
-        user.token = token
-        user.save()
-        return True
-    else:
+      user = User.objects.filter(id=id).first()
+      if user:
+          user.token = token
+          user.save()
+          return True
       return False
-    
+
 
 class getbyIdApi(APIView):
   def get(self, request, id=None):
@@ -195,75 +224,126 @@ class getbyIdApi(APIView):
         status=status.HTTP_400_BAD_REQUEST
       )
     
+
 class updateAPI(APIView):
-  def put(self, request, id=None, headers=None):
+  def put(self, request, id=None):
     response_data = {
-      'success': False,
-      'message': '',
-      'data': None
+    'success': False,
+    'message': '',
+    'data': None
     }
-    http_status = status.HTTP_400_BAD_REQUEST
+
     try:
-      if headers:
-        print("Headers:", headers)  
+      user = User.objects.get(id=id)
+    except User.DoesNotExist:
+      response_data['message'] = 'User not found'
+      return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
-      if id:
-        try:         
-          user = User.objects.get(id=id)
-          if('profile' in request.data):
-            profile_data = request.data.get('profile')
-            b64_string = profile_data 
-            filename = f"user_{id}_profile.jpg"
-            img_data = base64.b64decode(b64_string)
-            # print("Decoded image data:", img_data)  # For debugging purposes
-            profile_path = save_base64_image(b64_string, filename)
-            # print("Profile path:", profile_path)  # For debugging purposes
-          else:
-            filename = user.profile if user.profile else ""
+    profile_path = user.profile
+    if 'profile' in request.data:
+      profile_data = request.data.get('profile')
+      b64_string = profile_data 
+      filename = f"user_{id}_profile.jpg"
+      img_data = base64.b64decode(b64_string)
+      # print("Decoded image data:", img_data)  # For debugging purposes
+      profile_path = save_base64_image(b64_string, filename)
+      # print("Profile path:", profile_path)  # For debugging purposes
 
-          user_name = request.data.get('username', user.username)
-
-          if User.objects.filter(username=user_name).exclude(id=id).exists():
-              response_data['message'] = 'Username already exists'
-              return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
-          else:
-              user.username = user_name
-
-          email = request.data.get('email', user.email)
-          if User.objects.filter(email=email).exclude(id=id).exists():
-              response_data['message'] = 'Email already exists'
-              return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
-          else:
-              user.email = email
-
-          user.first_name = request.data.get('first_name', user.first_name)
-          user.last_name = request.data.get('last_name', user.last_name)
-          user.profile = profile_path if 'profile' in request.data else user.profile
-          user.updated_at = timezone.now()
-          user.save()
-          
-          response_data['success'] = True
-          response_data['message'] = 'User updated successfully'
-          response_data['data'] = {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'profile': user.profile,
-            'created_at': user.created_at.isoformat(),
-            'updated_at': user.updated_at.isoformat()
-          }
-          http_status = status.HTTP_200_OK
-        except User.DoesNotExist:
-          response_data['message'] = 'User not found'
-          http_status = status.HTTP_404_NOT_FOUND
-    except Exception as e:
-      response_data['message'] = str(e)
-      http_status = status.HTTP_400_BAD_REQUEST
+    #username validation
+    username = request.data.get('username', user.username)
+    if User.objects.filter(username=username).exclude(id=id).exists():
+      response_data['message'] = 'Username already exists'
+      return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
     
-    return Response(response_data, status=http_status)
+    valid_username, message = Validations().isvalidusername(username)
+    if not valid_username:
+      response_data['message'] = message
+      return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+    
+    #email validation
+    if 'email' in request.data:
+      user_email = request.data.get('email', user.email)
+      try:
+        validate_email(user_email)
+        email = user_email 
+      except ValidationError:
+        response_data['message'] = 'Invalid email format'
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+      if User.objects.filter(email=email).exclude(id=id).exists():
+        response_data['message'] = 'Email already exists'
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+    #first name and last name validation
+    if 'first_name' in request.data:
+      first_name = request.data.get('first_name', user.first_name)
+      valid_firstname, message = Validations().isvalidName(first_name)
+      if not valid_firstname:
+        response_data['message'] = 'Invalid first name: ' + message
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+    if 'last_name' in request.data:
+      last_name = request.data.get('last_name', user.last_name)
+      valid_lastname, message = Validations().isvalidName(last_name)
+      if not valid_lastname:
+        response_data['message'] = 'Invalid last name: ' + message
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+      
+    #password update
+    if ('password' in request.data):  
+      password = request.data.get('password')
+      user.password = make_password(password)
+
+    try:
+      user.username = username
+      user.email = email
+      user.first_name = request.data.get('first_name', user.first_name)
+      user.last_name = request.data.get('last_name', user.last_name)
+      user.password = user.password
+      user.profile = profile_path
+      user.updated_at = timezone.now()
+      user.save()
+    except IntegrityError:
+      response_data['message'] = 'Username or Email already exists'
+      return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+    response_data['success'] = True
+    response_data['message'] = 'User updated successfully'
+    response_data['data'] = {
+      'id': user.id,
+      'username': user.username,
+      'email': user.email,
+      'first_name': user.first_name,
+      'last_name': user.last_name,
+      'profile': user.profile,
+      'created_at': user.created_at.isoformat(),
+      'updated_at': user.updated_at.isoformat()
+    }
+
+    return Response(response_data, status=status.HTTP_200_OK)
 
 
+class Validations:
+  def isvalidusername(self, username):
+    message = ""
 
+    if username[0].isdigit() or username[0] == "_":
+      message = "Username cannot start with a number or underscore"
+      return (False, message)
+    
+    if "@" in username:
+      message = "Username cannot contain '@'"
+      return (False, message)
 
+    # if not re.match(r'^[A-Za-z0-9_-]+$', username):
+    #   message = "Username can only contain letters, numbers, underscores, or hyphens"
+    #   return (False, message)
+
+    return (True, "Valid username")
+    
+  def isvalidName(name):
+      message = ""
+      if name[0].isdigit() or name[0] == "_":
+        message = "Username cannot start with a number or underscore"
+        return (False, message)
+      
+      return (True, "Valid name")
