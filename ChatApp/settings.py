50afb68a16f034173eefb550b9142a1f95aef7c3
Misbah Sehar <misbahsahar09@gmail.com>
2026-01-15 11:48:29 +0500

    cors

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'ChatApp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,3 +142,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
